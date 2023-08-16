94f087558ce9cca9be07c9ad012f81cafd4252b4
PhuongNV <ducanh96691996@gmail.com>
2023-08-16 11:12:20 +0700

    commit done 16/8/2023/lan1

--- force-app/main/default/lwc/demo_Bai2/demo_Bai2.js
+++ force-app/main/default/lwc/demo_Bai2/demo_Bai2.js
@@ -1,9 +1,9 @@
 import { LightningElement, wire } from 'lwc';
-import getTotalTickets from '@salesforce/apex/VMO_CaseTriggerHandler.TotalTickets';
-import getTotalThisMonth from '@salesforce/apex/VMO_CaseTriggerHandler.TotalThisMonth';
-import getTotalOpen from '@salesforce/apex/VMO_CaseTriggerHandler.TotalOpen';
-import getTotalProcessing from '@salesforce/apex/VMO_CaseTriggerHandler.TotalProcessing';
-import getTotalClosed from '@salesforce/apex/VMO_CaseTriggerHandler.TotalClosed';
+import getTotalTickets from '@salesforce/apex/VMO_SummaryCaseTriggerHandler.getTotalTickets';
+import getTotalThisMonth from '@salesforce/apex/VMO_SummaryCaseTriggerHandler.getTotalThisMonth';
+import getTotalOpen from '@salesforce/apex/VMO_SummaryCaseTriggerHandler.getTotalOpen';
+import getTotalProcessing from '@salesforce/apex/VMO_SummaryCaseTriggerHandler.getTotalProcessing';
+import getTotalClosed from '@salesforce/apex/VMO_SummaryCaseTriggerHandler.getTotalClosed';
 
 export default class Demo_Bai2 extends LightningElement {
     total_case;
@@ -17,10 +17,14 @@ export default class Demo_Bai2 extends LightningElement {
     }
 
     async fetchUserData() {
-        this.total_case = await getTotalTickets();
-        this.total_month = await getTotalThisMonth();
-        this.total_open = await getTotalOpen();
-        this.total_processing = await getTotalProcessing();
-        this.total_closed = await getTotalClosed();
+        try {
+            this.total_case = await getTotalTickets() || 0;
+            this.total_month = await getTotalThisMonth() || 0;
+            this.total_open = await getTotalOpen() || 0;
+            this.total_processing = await getTotalProcessing() || 0;
+            this.total_closed = await getTotalClosed() || 0;
+        } catch (error) {
+            console.error('An error occurred:', error);
+        }
     }
 }
